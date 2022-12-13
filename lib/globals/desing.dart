b96e9f55090b3d95cde2a325814700db6f34aa6b
Rabia Nur Özdemir <rabianur.ozdemir@gazi.edu.tr>
2022-12-13 13:39:39 +0300

    Sign in and sign up completed

--- lib/globals/desing.dart
+++ lib/globals/desing.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/painting.dart';
 
 class BridgeColors {
-  static Color primaryColor = Color.fromRGBO(186, 104, 200, 1);
-  static Color secondaryColor = Color.fromARGB(255, 107, 63, 115);
+  static Color primaryColor = const Color.fromRGBO(186, 104, 200, 1);
+  static Color secondaryColor = const Color.fromARGB(255, 107, 63, 115);
+   
 }
--- lib/login.dart
+++ lib/login.dart
@@ -1,3 +1,7 @@
+import 'dart:convert';
+
+import 'package:fluttertoast/fluttertoast.dart';
+
 import 'globals/desing.dart';
 import 'dart:async';
 import 'package:flutter/material.dart';
@@ -11,16 +15,25 @@ class Login extends StatefulWidget {
   State<Login> createState() => _LoginState();
 }
 
-final values = [
-  "../assets/images/slideimage1.png",
-  "../assets/images/slideimage2.png",
-  "../assets/images/slideimage3.png",
-  "../assets/images/slideimage4.png"
-];
-int _index = 0;
-
 class _LoginState extends State<Login> {
   late final Timer timer;
+
+  final values = [
+    "../assets/images/slideimage1.png",
+    "../assets/images/slideimage2.png",
+    "../assets/images/slideimage3.png",
+    "../assets/images/slideimage4.png"
+  ];
+
+  int _index = 0;
+  final _formKey = GlobalKey<FormState>();
+  final _nameController = TextEditingController();
+  final _surnameController = TextEditingController();
+  final _mailController = TextEditingController();
+  final _passwordController = TextEditingController();
+  bool _isLogin = true;
+  bool _isDisabled = false;
+
   @override
   void initState() {
     super.initState();
@@ -35,12 +48,6 @@ class _LoginState extends State<Login> {
     super.dispose();
   }
 
-  final _formKey = GlobalKey<FormState>();
-  final _mailController = TextEditingController();
-  final _passwordController = TextEditingController();
-//sorun yok
-//sorun yok
-//sorun yok
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -52,94 +59,86 @@ class _LoginState extends State<Login> {
             // MediaQuery.of(context).size.width >= 980
             //     ? Menu()
             //     : SizedBox(), // Responsive
-            Body(),
+            Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                if (MediaQuery.of(context).size.width >= 1200) ...[
+                  SizedBox(
+                    width: 360,
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        const Text(
+                          'Hoş geldiniz!',
+                          style: TextStyle(
+                            fontSize: 45,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        AnimatedSwitcher(
+                          duration: const Duration(milliseconds: 500),
+                          child: Image.asset(
+                            values[_index %
+                                values
+                                    .length], // manually change the text here, and hot reload
+                            key: UniqueKey(),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  const SizedBox(
+                    width: 200,
+                  ),
+                ],
+                // MediaQuery.of(context).size.width >= 1300 //Responsive
+                //     ? Image.asset(
+                //         'images/illustration-1.png',
+                //         width: 300,
+                //       )
+                //     : SizedBox(),
+                Container(
+                  padding:
+                      const EdgeInsets.symmetric(horizontal: 40, vertical: 50),
+                  decoration: BoxDecoration(
+                    border: Border.all(color: BridgeColors.secondaryColor),
+                    borderRadius: BorderRadius.circular(8),
+                  ),
+                  child: SizedBox(
+                    width: 300,
+                    child: _isLogin ? _formSignIn() : _formSignUp(),
+                  ),
+                )
+              ],
+            ),
+          ],
+        ),
       ),
     );
   }
 
-  _onSignUpPressed() {
-    Navigator.push(
-      context,
-      MaterialPageRoute(builder: (context) => const SignUp()),
-    );
-  }
-
-  _onLoginButtonPressed() async {
-    if (!(_formKey.currentState?.validate() ?? false)) {
-      return;
-    }
-  }
-}
-
-class Body extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
+  Widget _formSignIn() {
     return Column(
       mainAxisAlignment: MainAxisAlignment.center,
       children: [
-        Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            if (MediaQuery.of(context).size.width >= 1200) ...[
-              SizedBox(
-                width: 360,
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: [
-                    AnimatedSwitcher(
-                      duration: const Duration(milliseconds: 500),
-                      child: Image.asset(
-                        values[_index %
-                            values
-                                .length], // manually change the text here, and hot reload
-                        key: UniqueKey(),
-                      ),
-                    ),
-                    const Text(
-                      'Hoşgeldin',
-                      style: TextStyle(
-                        fontSize: 45,
-                        fontWeight: FontWeight.bold,
-                      ),
-                    )
-                  ],
-                ),
+        SizedBox(
+          child: Padding(
+            padding: EdgeInsets.only(bottom: 50),
+            child: Text(
+              'Bridge',
+              style: TextStyle(
+                fontSize: 40,
+                color: BridgeColors.secondaryColor,
               ),
-              const SizedBox(
-                width: 200,
-              ),
-            ],
-            // MediaQuery.of(context).size.width >= 1300 //Responsive
-            //     ? Image.asset(
-            //         'images/illustration-1.png',
-            //         width: 300,
-            //       )
-            //     : SizedBox(),
-            Container(
-              padding:
-                  const EdgeInsets.symmetric(horizontal: 40, vertical: 150),
-              decoration: BoxDecoration(
-                border: Border.all(color: BridgeColors.secondaryColor),
-                borderRadius: BorderRadius.circular(8),
-              ),
-              child: SizedBox(
-                width: 320,
-                child: _formLogin(),
-              ),
-            )
-          ],
+            ),
+          ),
         ),
-      ],
-    );
-  }
-
-  Widget _formLogin() {
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
         TextField(
           decoration: InputDecoration(
-            hintText: 'Enter email or Phone number',
+            hintText: 'E-mail',
             filled: true,
             fillColor: Colors.blueGrey[50],
             labelStyle: const TextStyle(fontSize: 12),
@@ -157,8 +156,8 @@ class Body extends StatelessWidget {
         const SizedBox(height: 30),
         TextField(
           decoration: InputDecoration(
-            hintText: 'Password',
-            counterText: 'Forgot password?',
+            hintText: 'Şifre',
+            counterText: 'Şifremi unuttum',
             suffixIcon: const Icon(
               Icons.visibility_off_outlined,
               color: Colors.grey,
@@ -198,9 +197,9 @@ class Body extends StatelessWidget {
                 child: Center(child: Text("Giriş Yap"))),
           ),
         ),
-        const SizedBox(height: 40),
+        const SizedBox(height: 20),
         TextButton(
-          onPressed: _onSignUpButtpnPressed,
+          onPressed: _changeForm,
           child: Text("Hesabın yok mu? Hemen Kaydol",
               style: TextStyle(color: BridgeColors.primaryColor)),
         ),
@@ -208,47 +207,175 @@ class Body extends StatelessWidget {
     );
   }
 
-  _onSignUpButtpnPressed() {}
-  Widget _loginWithButton({String? image, bool isActive = false}) {
-    return Container(
-      width: 90,
-      height: 70,
-      decoration: isActive
-          ? BoxDecoration(
-              color: Colors.white,
-              boxShadow: [
-                BoxShadow(
-                  color: Colors.grey[300]!,
-                  spreadRadius: 10,
-                  blurRadius: 30,
-                )
-              ],
-              borderRadius: BorderRadius.circular(15),
-            )
-          : BoxDecoration(
-              borderRadius: BorderRadius.circular(15),
-              border: Border.all(color: Colors.grey[400]!),
+  Widget _formSignUp() {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        SizedBox(
+          child: Padding(
+            padding: EdgeInsets.only(bottom: 30),
+            child: Text(
+              'Bridge',
+              style: TextStyle(
+                fontSize: 40,
+                color: BridgeColors.secondaryColor,
+              ),
             ),
-      child: Center(
-          child: Container(
-        decoration: isActive
-            ? BoxDecoration(
-                color: Colors.white,
-                borderRadius: BorderRadius.circular(35),
-                boxShadow: [
-                  BoxShadow(
-                    color: Colors.grey[400]!,
-                    spreadRadius: 2,
-                    blurRadius: 15,
-                  )
-                ],
-              )
-            : const BoxDecoration(),
-        child: Image.asset(
-          '$image',
-          width: 35,
+          ),
         ),
-      )),
+        TextField(
+          controller: _nameController,
+          decoration: InputDecoration(
+            hintText: 'İsim',
+            filled: true,
+            fillColor: Colors.blueGrey[50],
+            labelStyle: const TextStyle(fontSize: 12),
+            contentPadding: const EdgeInsets.only(left: 30),
+            enabledBorder: OutlineInputBorder(
+              borderSide: const BorderSide(color: Colors.grey),
+              borderRadius: BorderRadius.circular(15),
+            ),
+            focusedBorder: OutlineInputBorder(
+              borderSide: BorderSide(color: BridgeColors.secondaryColor),
+              borderRadius: BorderRadius.circular(15),
+            ),
+          ),
+        ),
+        const SizedBox(height: 15),
+        TextField(
+          controller: _surnameController,
+          decoration: InputDecoration(
+            hintText: 'Soy isim',
+            filled: true,
+            fillColor: Colors.blueGrey[50],
+            labelStyle: const TextStyle(fontSize: 12),
+            contentPadding: const EdgeInsets.only(left: 30),
+            enabledBorder: OutlineInputBorder(
+              borderSide: const BorderSide(color: Colors.grey),
+              borderRadius: BorderRadius.circular(15),
+            ),
+            focusedBorder: OutlineInputBorder(
+              borderSide: BorderSide(color: BridgeColors.secondaryColor),
+              borderRadius: BorderRadius.circular(15),
+            ),
+          ),
+        ),
+        const SizedBox(height: 15),
+        TextField(
+          controller: _mailController,
+          decoration: InputDecoration(
+            hintText: 'E-mail',
+            filled: true,
+            fillColor: Colors.blueGrey[50],
+            labelStyle: const TextStyle(fontSize: 12),
+            contentPadding: const EdgeInsets.only(left: 30),
+            enabledBorder: OutlineInputBorder(
+              borderSide: const BorderSide(color: Colors.grey),
+              borderRadius: BorderRadius.circular(15),
+            ),
+            focusedBorder: OutlineInputBorder(
+              borderSide: BorderSide(color: BridgeColors.secondaryColor),
+              borderRadius: BorderRadius.circular(15),
+            ),
+          ),
+        ),
+        const SizedBox(height: 15),
+        TextField(
+          controller: _passwordController,
+          decoration: InputDecoration(
+            hintText: 'Şifre',
+            filled: true,
+            fillColor: Colors.blueGrey[50],
+            labelStyle: const TextStyle(fontSize: 12),
+            contentPadding: const EdgeInsets.only(left: 30),
+            enabledBorder: OutlineInputBorder(
+              borderSide: const BorderSide(color: Colors.grey),
+              borderRadius: BorderRadius.circular(15),
+            ),
+            focusedBorder: OutlineInputBorder(
+              borderSide: BorderSide(color: BridgeColors.secondaryColor),
+              borderRadius: BorderRadius.circular(15),
+            ),
+          ),
+        ),
+        const SizedBox(height: 20),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Checkbox(
+              shape: CircleBorder(),
+              checkColor: Colors.white,
+              activeColor: BridgeColors.primaryColor,
+              value: _isDisabled,
+              onChanged: (bool? value) {
+                setState(() {
+                  _isDisabled = value!;
+                });
+              },
+            ),
+            const Text("Engelli bir bireyim."),
+          ],
+        ),
+        const SizedBox(height: 20),
+        Container(
+          decoration: BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.circular(30),
+          ),
+          child: ElevatedButton(
+            onPressed: () => print("it's pressed"),
+            style: ElevatedButton.styleFrom(
+              foregroundColor: Colors.white,
+              backgroundColor: BridgeColors.primaryColor,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(15),
+              ),
+            ),
+            child: const SizedBox(
+                width: double.infinity,
+                height: 50,
+                child: Center(child: Text("Kayıt Ol"))),
+          ),
+        ),
+        const SizedBox(height: 20),
+        TextButton(
+          onPressed: _changeForm,
+          child: Text("Hesabın var mı? Giriş yap.",
+              style: TextStyle(color: BridgeColors.primaryColor)),
+        ),
+      ],
     );
+  }
+
+  _changeForm() {
+    setState(() {
+      _isLogin = !_isLogin;
+    });
+  }
+
+  _onSignUpButtonPressed() async {
+    final url = "http://localhost:8000/appusers";
+    try {
+      Fluttertoast.showToast(
+          msg: "This is Center Short Toast",
+          toastLength: Toast.LENGTH_SHORT,
+          gravity: ToastGravity.CENTER,
+          timeInSecForIosWeb: 1,
+          backgroundColor: Colors.red,
+          textColor: Colors.white,
+          fontSize: 16.0);
+      var response = await http.post(
+        Uri.parse(url),
+        body: jsonEncode(<String, String>{
+          'first_name': _nameController.text,
+          'last_name': _surnameController.text,
+          'email': _mailController.text,
+          'is_disabled': _isDisabled.toString(),
+          'password': _passwordController.text,
+        }),
+      );
+    } catch (e) {
+      print(e);
+    }
   }
 }
